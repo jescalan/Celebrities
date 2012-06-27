41e979c0b2ef9425ed551178bd052d2ec785e3ae
Jeff Escalante <hello@jenius.me>
2012-06-27 10:33:23 -0400

    it's all good baby baby

---
+++ music/juicy.js
@@ -0,0 +1,49 @@
+biggie = Notorious.new("BIG")
+you = Person.new(self)
+
+var it = "all a dream"
+biggie.read("Word Up Magazine")
+limosine = ["Salt & Pepper", "Heavy D"]
+wall.hang("pictures")
+if ((new Date).toString().substr(0,3) == "Sat") {
+  var rap_attack = Array.new
+  rap_attack.push("Mr. Magic", "Marley Marl")
+  console.log(rap_attack)
+}
+while(tape.popped == false) {
+  tape.rock()
+}
+var way_back = new Date(1/12/1987)
+while(biggie.has("red & black lumberjack").with("hat to match")) {
+  biggie.smoke("weed", "bamboo")
+  biggie.sip("private stock")
+}
+you.recall("Rappin Duke")
+for(var i = 0; i < 2; i++) {
+  console.log("duh hah")
+}
+you.thoughts = "hip hop will make it this far"
+you.thoughts = false
+if (biggie.rhymes_tight == true) {
+  limelight.push(biggie)
+}
+current_time = new Time('get paid')
+biggie.blow_up().match(/world\strade/)
+var born_sinner = biggie
+if (born_sinner !== winner) {
+  biggie.eat("sardines", "dinner", way_back)
+}
+send_peace = function(people) {
+  for(var i = 0; i < people.length; i++) {
+    people[i].send("peace")
+  }
+}
+candidates = ["Ron G", "Brucey B", "Kid Capri"]
+candidates.push("Funkmaster Flex", "Lovebug Starsky")
+send_peace(candidates)
+you.thoughts = "biggie is gonna blow up"
+you.thoughts = true
+you.call(biggie.crib)
+var now = (new Date).getTime()
+biggie.number(way_back) == biggie.number(now) && biggie.hood(way_back) == biggie.hood(now)
+var all = "good"
